d6844841862f0a47e0b7c671ed5e29a58c0d3bda
Michael Boles <michaelboles@Michaels-MacBook-Pro.local>
2019-04-28 14:43:14 -0700

    Continued work on copyspecial.py

--- copyspecial-mb.py
+++ copyspecial-mb.py
@@ -10,15 +10,46 @@ import sys
 import re
 import os
 import shutil
-import commands
+#import commands
 
 """Copy Special exercise
 """
 
-# +++your code here+++
+#directory_name = '/Users/michaelboles/Michael/Coding/2019/Google-Python/google-python-exercises/copyspecial'
+
 # Write functions and modify main() to call them
 
+def get_special_paths(directory_name):
+    
+    files = os.listdir(directory_name) # create array with filenames contained in given directory
+    special_paths = []                 # initiate empty array to populate in for loop
 
+    for file in files:                         # loop over files in directory
+        special = re.search(r'(\w+__\w+__\.\w+)', file) 
+                                               # find: '(letters)(__)(letters)(__)(.)(letters)'
+        if special:
+            special_filename = special.group(1)
+                                               # if match found, create string object of filename
+            special_path = os.path.abspath(os.path.join(directory_name, special_filename))
+                                               # then join path of directory and file 
+            special_paths.append(special_path) # tack onto end of existing path list
+    return(special_paths)
+
+#get_special_paths(directory_name)
+
+paths = special_paths
+destination = '/Users/michaelboles/Desktop/Copyspecial'
+
+def copy_to(paths, destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    for path in paths:
+        basename = os.path.basename(path)
+        if not os.path.exists(os.path.join(destination, basename)):
+            shutil.copy(path, os.path.join(destination, basename))
+        
+def zip_to(paths, zip_path):
+    
 
 def main():
   # This basic command line argument parsing code is provided.
