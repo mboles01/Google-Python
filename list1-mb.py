faebfecd320a0c6820a9069b5042675537f9c894
Michael Boles <michaeladamboles@gmail.com>
2019-05-02 17:55:46 -0700

    Completed list1.py

--- list1-mb.py
+++ list1-mb.py
@@ -61,7 +61,7 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
     
-tuples = [(1, 7), (1, 3), (3, 4, 5), (2, 2)]
+#tuples = [(1, 7), (1, 3), (3, 4, 5), (2, 2)]
 
 def last(tuple):
     return tuple[-1]
